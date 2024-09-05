48f29c26decf0b1e7ff0157a38b11d605134df11
Sumit Purbey <sumitpurbey101@gmail.com>
2024-09-05 12:22:59 +0545

    Dependencies updated

---
+++ lib/data/data.dart
@@ -0,0 +1,123 @@
+class Data {
+  final int id;
+  final String title, artist, url, thumb, asset;
+
+  Data({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.url,
+    required this.thumb,
+    required this.asset,
+  });
+}
+
+List<Data> datas = [
+  Data(
+    id: 1,
+    title: "Tum Hardafa Ho",
+    artist: "Ankit Tiwari",
+    asset: 'assets/audios/hardafa.mp3',
+    thumb: "assets/images/tumhardafa.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/hardafa.mp3',
+  ),
+  Data(
+    id: 2,
+    title: "Soniyo Version 2.0",
+    artist: "Adhyayan Suman",
+    asset: 'assets/audios/soniyo.mp3',
+    thumb: "assets/images/soniyo2.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/soniyo.mp3',
+  ),
+  Data(
+    id: 3,
+    title: "Genda Phool",
+    artist: "Badshah ft. Payal Dev",
+    asset: 'assets/audios/genda.mp3',
+    thumb: "assets/images/genda.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/genda.mp3',
+  ),
+  Data(
+    id: 4,
+    title: "Malang (Title Track)",
+    artist: "Ved Sharma",
+    asset: 'assets/audios/malang.mp3',
+    thumb: "assets/images/malang.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/malang.mp3',
+  ),
+  Data(
+    id: 5,
+    title: "Kehndi Ha Kehndi Na",
+    artist: "Shukriti & Prakriti",
+    asset: 'assets/audios/kehndi.mp3',
+    thumb: "assets/images/kehndi.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/kehndi.mp3',
+  ),
+  Data(
+    id: 6,
+    title: "Khuch Bhi ho Jaye",
+    artist: "B Praak",
+    asset: 'assets/audios/kuchbhi.mp3',
+    thumb: "assets/images/kuch.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/kuchbhi.mp3',
+  ),
+  Data(
+    id: 7,
+    title: "Asal Mein",
+    artist: "Darshan Raval",
+    asset: 'assets/audios/asal.mp3',
+    thumb: "assets/images/asal.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/asal.mp3',
+  ),
+  Data(
+    id: 8,
+    title: "Chal Ghar Chalen",
+    artist: "Arijit Singh",
+    asset: 'assets/audios/chal.mp3',
+    thumb: "assets/images/malang.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/audios/chal.mp3',
+  ),
+];
+
+class Video {
+  final int id;
+  final String title, artist, url, thumb, asset;
+
+  Video({
+    required this.id,
+    required this.title,
+    required this.artist,
+    required this.url,
+    required this.thumb,
+    required this.asset,
+  });
+}
+
+List<Video> videos = [
+  Video(
+    id: 1,
+    title: "Phir Na Milen Kabhi",
+    artist: "Ankit Tiwari",
+    asset: 'assets/videos/hum.mp4',
+    thumb: "assets/images/hum2.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/videos/hum.mp4',
+  ),
+  Video(
+    id: 2,
+    title: "Kurta Pajama",
+    artist: "Tony Kakkar",
+    asset: 'assets/videos/kurta.mp4',
+    thumb: "assets/images/kurta.jpg",
+    url:
+        'https://raw.githubusercontent.com/shivam1808/Beats-Audio-Video-Player/master/assets/videos/hum.mp4',
+  ),
+];
